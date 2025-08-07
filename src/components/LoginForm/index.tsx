cf71202ebb18fc1748f4cbc87b679d9f9cc000e0
abnerpp <arierap@gmail.com>
2025-08-06 21:34:46 -0300

    Inputs e botões de login

---
+++ src/components/LoginForm/index.tsx
@@ -0,0 +1,145 @@
+import { useState } from "react";
+import Link from "next/link";
+import { CiMail } from "react-icons/ci";
+import { GoLock } from "react-icons/go";
+import { IoEyeOutline } from "react-icons/io5";
+import { IoEyeOffOutline } from "react-icons/io5";
+import { FiGithub } from "react-icons/fi";
+import { PiGoogleChromeLogoBold } from "react-icons/pi";
+
+export default function LoginForm() {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <div
+        className="bg-gradient-to-br 
+                from-[#181b20cc] to-[#1d2025e6] 
+                    backdrop-blur-lg border border-[#2c313a]/50
+                        rounded-lg"
+      >
+        <div className="flex flex-col p-6 text-center">
+          <h1 className="text-2xl font-semibold bg-gradient-to-r from-[#5593f7] to-[#1d4fd7] text-transparent bg-clip-text">
+            Bemvindo de volta
+          </h1>
+          <p className="text-sm text-gray-400">
+            Entre na sua conta para continuar
+          </p>
+        </div>
+        <div className="pt-0 p-6 space-y-6">
+          <form onSubmit={() => {}} className="space-y-4">
+            <div className="space-y-2">
+              <label
+                htmlFor="email"
+                className="text-sm font-medium text-[#f1f2f3]"
+              >
+                Email
+              </label>
+              <div className="relative">
+                <CiMail
+                  className="absolute text-gray-400 left-3 top-1/2 transform -translate-y-1/2"
+                  size={20}
+                />
+
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="seu@email.com"
+                  required
+                  className="bg-[#21252b] border border-[#2c313a]/50
+                            focus:border-[#5593f7] outline-none rounded-md py-2 w-full
+                            placeholder:text-gray-500 text-[#f1f2f3] pl-10"
+                />
+              </div>
+            </div>
+
+            <div className="space-y-2">
+              <label
+                htmlFor="password"
+                className="text-sm font-medium text-[#f1f2f3]"
+              >
+                Senha
+              </label>
+              <div className="relative">
+                <GoLock
+                  className="absolute text-gray-400 left-3 top-1/2 transform-translate-y-1/2"
+                  size={20}
+                />
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  placeholder="********"
+                  required
+                  className="bg-[#21252b] border border-[#2c313a]/50
+                            focus:border-[#5593f7] outline-none rounded-md py-2 w-full
+                            placeholder:text-gray-500 text-[#f1f2f3] px-10"
+                />
+                <div
+                  onClick={handleShowPassword}
+                  className="absolute text-gray-400 right-3 top-1/2 transform-translate-y-1/2 text-[20px] cursor-pointer hover:text-withe transition-colors"
+                >
+                  {showPassword ? <IoEyeOffOutline /> : <IoEyeOutline />}
+                </div>
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <label className="flex items-center gap-2 text-sm">
+                <input
+                  type="checkbox"
+                  className="w-4 h-4 rouded-sm border border-[#2c313a]/50 text-[#5593f7]"
+                />
+                <span className="text-gray-400">Lembrar-me</span>
+              </label>
+              <Link
+                href="/forgot-password"
+                className="text-sm text-[#5593f7] hover:text-[#5593f7]/80 transition-colors"
+              >
+                Esqueceu a senha?
+              </Link>
+            </div>
+            <button
+              type="submit"
+              className="text-lg font-medium rounded-xl bg-gradient-to-r
+                             from-[#5593f7] to-[#1d47d7] w-full h-[50px]
+                             hover:shadow-xl hover:shadow-[#4896ff26] 
+                             transition-all duration-300 ease-in-out cursor-pointer"
+            >
+              Entrar
+            </button>
+          </form>
+          <div className="w-full flex items-center justify-center">
+            <div className="w-[35%] h-[1px] bg-[#2c313a]"></div>
+            <p className=" text-[12px] text-gray-400">CONTINUE COM</p>
+            <div className="w-[35%] h-[1px] bg-[#2c313a]"></div>
+          </div>
+          <div className="grid grid-cols-2 gap-3">
+            <button
+              className="border border-[#2C313A]/50 rounded-xl bg-[#111418] h-[45px] text-[#F1F2F3] flex items-center justify-center gap-4
+             hover:bg-[#F3991D] hover:text-[#111418] transition-all duration-300 ease-in-out"
+            >
+              <FiGithub size={20} />
+              <span className="text-sm font-medium">GitHub</span>
+            </button>
+
+            <button
+              className="border border-[#2C313A]/50 rounded-xl bg-[#111418] h-[45px] text-[#F1F2F3] flex items-center justify-center gap-4
+             hover:bg-[#4889F4] hover:text-[#111418] transition-all duration-300 ease-in-out"
+            >
+              <PiGoogleChromeLogoBold size={20} />
+              <span className="text-sm font-medium">Google</span>
+            </button>
+          </div>
+          <div className="text-center text-sm text-gray-400 flex gap-1 justify-center items-center">
+            <p>Não tem uma conta?</p>
+            <Link href="/registrer" className="text-[#5593F7] hover:text-[#5593F7]/80 transition-colors font-medium">
+            Cadastre-se</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
